0cdf8bbf373dc4538db8f97c2007e6ddd628e7d6
Dibyasmita-Mohapatra <dibyasmita.m05@gmail.com>
2026-07-05 13:58:28 +0530

    Build AI Interview Assistant with React, Node.js, Express, and MongoDB

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
     try {
 
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://ai-coding-interview-assistant-1.onrender.com/api/auth/login",
         formData
       );
 
