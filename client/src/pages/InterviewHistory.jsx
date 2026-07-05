d800955970350b3cdb2845e5977fad86abaf5b05
Dibyasmita-Mohapatra <dibyasmita.m05@gmail.com>
2026-07-05 13:55:05 +0530

    Build AI Interview Assistant with React, Node.js, Express, and MongoDB

--- client/src/pages/InterviewHistory.jsx
+++ client/src/pages/InterviewHistory.jsx
@@ -16,7 +16,7 @@ const InterviewHistory = () => {
 
     const response =
       await axios.get(
-        "http://localhost:5000/api/interview/all"
+        "https://ai-coding-interview-assistant-1.onrender.com/api/interview/all"
       );
 
     setInterviews(response.data);
